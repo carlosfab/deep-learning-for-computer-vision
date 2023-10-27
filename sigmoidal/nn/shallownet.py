d2deeadca98952a738ad101d62b29d56ba10a01b
Carlos Melo <carlos.fab@gmail.com>
2023-10-26 21:49:27 -0300

    ShallowNet created

---
+++ sigmoidal/nn/shallownet.py
@@ -0,0 +1,42 @@
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Activation, Flatten
+from keras import backend as K
+
+
+class ShallowNet:
+    """
+    ShallowNet is a class that builds a shallow neural network for image classification.
+    """
+    @staticmethod
+    def build(width, height, depth, classes):
+        """
+        Builds a shallow neural network for image classification.
+
+        Parameters:
+        -----------
+        width : int
+            The width of the input image.
+        height : int
+            The height of the input image.
+        depth : int
+            The depth of the input image.
+        classes : int
+            The number of classes to classify the input image into.
+
+        Returns:
+        --------
+        model : keras.Sequential
+            The shallow neural network model.
+        """
+        model = Sequential()
+        input_shape = (height, width, depth)
+
+        model.add(Conv2D(32, (3, 3), padding="same", input_shape=input_shape))
+        model.add(Activation("relu"))
+        model.add(Flatten())
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
+
+        # return the network
+        return model
